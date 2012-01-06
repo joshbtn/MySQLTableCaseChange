fc23811895c55cee74182216816c4f35ebb11b23
Josh Bennett <josh@j-ben.com>
2012-01-06 13:15:15 -0500

    working on js still

--- js/index.js
+++ js/index.js
@@ -1,17 +1,48 @@
 (function(){
     
-    //Handle convert clicked
-    function convert(sql, e) {
-		var matchCreateTable = /create\s+table\s+.([^`]+)./gi,
-            matchesForCreate = sql.match(matchCreateTable),
-            tableNamesArray = [];
-        debugger;
+    // Handle convert clicked
+    function convert(sql, toUpperCase, e) {
+		
+		// Make sure toUpperCase is a boolean. If it's not default to false.
+		toUpper = (toUpper === true ||  toUpper === false) ? toUpper : false;
+		
+		// RegExp to Match the 'create' statment to get table names
+		var matchCreateTable = new RegExp("create\\s+table\\s+`[^`]+`", "gi"),
+		
+		// Store matches for the create statments
+			matchesForCreate = sql.match(matchCreateTable),
+		
+		// RegExp to Match the table names
+			matchTableName = new RegExp("`[^`]+`"),
+		
+		//This will hold the original table names		
+            originalNames = [],
+		
+		//This will hold the new tableNames
+			newNames = [],
+		
+		//These are used in the for loop
+			curStr = "",
+			curIndex = 0;
+		
+		//Loop through all create statments to pull out the table names
+		for(; curIndex < matchesForCreate.length; curIndex+=1 ){
+			
+			//set the current create stament
+			curStr = matchesForCreate[curIndex];
+			
+			
+			originalNames.push(curStr.match(matchTableName)[0]);
+			
+			newNames.push( curStr.match(matchTableName)[0].toString()[toUpper ? "toUpperCase" : "toLowerCase"]() );
+		}
+		debugger;
     }
         
     //DOM LOAD
     //----------
     $(function(){
-        var io = $('#io'),
+        var $io = $('#io'),
             $convertbutton = $('#convertButton');
         
         //Event Bindings
