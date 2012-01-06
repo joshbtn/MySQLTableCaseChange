e11c801c182cf7e9712dc551abbf5fb2136dcb7b
Josh Bennett <josh@j-ben.com>
2012-01-06 16:04:46 -0500

    pretty it up, still need to check into why drop isn't getting added twice

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 #MySQLTableCaseChange
+As of right now all the magic happens in js/index.js.  I do plan on creating a command line tool to help with some scripts I have.  This all came about while trying to push Umbraco from my local windows dev environment to Linux shared hosting.
--- js/index.js
+++ js/index.js
@@ -1,10 +1,11 @@
 (function(){
     
-    // Handle convert clicked
-    function convert(sql, toUpperCase, e) {
+    // Convert takes the sql text from mysqldump, and a toUppercase boolean value.
+	// If toUpperCase is set to false then names will be converted to lower case.
+    function convert(sql, toUpperCase) {
 		
 		// Make sure toUpperCase is a boolean. If it's not default to false.
-		toUpper = (toUpper === true ||  toUpper === false) ? toUpper : false;
+		toUpperCase = (toUpperCase === true ||  toUpperCase === false) ? toUpperCase : false;
 		
 		// RegExp to Match the 'create' statment to get table names
 		var matchCreateTable = new RegExp("create\\s+table\\s+`[^`]+`", "gi"),
@@ -15,42 +16,65 @@
 		// RegExp to Match the table names
 			matchTableName = new RegExp("`[^`]+`"),
 		
-		//This will hold the original table names		
-            originalNames = [],
+		// This will hold a map from the old names to the new tableNames
+			nameMap = {},
 		
-		//This will hold the new tableNames
-			newNames = [],
+		// RegExp to match the current name.
+			matchToReplace = null,
 		
-		//These are used in the for loop
+		// RegExp to match drop statements.  Seemed useful to me to drop the new case, and 
+			matchDropToDup = null,
+		
+		// Output string
+			outStr = sql,
+			
+		// _These are used in the for loop_
 			curStr = "",
-			curIndex = 0;
-		
+			curIndex = 0,
+			curTableName = "",
+			newName ="";
+			
 		//Loop through all create statments to pull out the table names
 		for(; curIndex < matchesForCreate.length; curIndex+=1 ){
 			
-			//set the current create stament
+			// Set the current create stament
 			curStr = matchesForCreate[curIndex];
 			
+			//Set the current table name
+			curTableName = curStr.match(matchTableName)[0];
 			
-			originalNames.push(curStr.match(matchTableName)[0]);
+			//Set the new table name.
+			newName = curTableName.replace('`', '')[toUpperCase ? "toUpperCase" : "toLowercase"]();
 			
-			newNames.push( curStr.match(matchTableName)[0].toString()[toUpper ? "toUpperCase" : "toLowerCase"]() );
+			//Create a mapping of all changed names.
+			nameMap[curTableName] = newName;
+			
+			// Replace original table name with new tablename
+			matchToReplace = new RegExp(curTableName, "gi");
+			outStr = outStr.replace(matchToReplace, newName);
+			
+			// Drop both the new name, and the originalName
+			matchDropToDup = new RegExp ("(DROP\\s+TABLE[^`]+`" + newName + "+`\\s*;)", "gi");
+			debugger;
+			outStr = outStr.replace(matchDropToDup, "\1\n" + "DROP TABLE IF EXISTS `" + curTableName + "`;");
 		}
+		
 		debugger;
+		
+		return outStr;
     }
         
-    //DOM LOAD
-    //----------
+    // DOM LOAD
+    // ----------
     $(function(){
         var $io = $('#io'),
             $convertbutton = $('#convertButton');
         
-        //Event Bindings
+        //Handle convert button clicked
         $convertbutton.bind('click', function(e){
-			convert($io.val(), e);
+			$io.val(convert($io.val(), true));
 			return false;
         });
-        
     });
     
 }());
