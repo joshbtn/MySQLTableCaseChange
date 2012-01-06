0f05c093b5dc89060d7682cd7c7692b3617e6359
Josh Bennett <josh@j-ben.com>
2012-01-06 16:59:29 +0000

    js

---
+++ js/index.js
@@ -0,0 +1,24 @@
+(function(){
+    
+    //Handle convert clicked
+    function convert(sql, e) {
+        var matchCreateTable = /create\s+table\s+.([^`]+)./gi,
+            matchesForCreate = sql.match(matchCreateTable);
+            tableNamesArray = [];
+        debugger;
+    }
+        
+    //DOM LOAD
+    //----------
+    $(function(){
+        var $io = $('#io'),
+            $convertbutton = $('#convertButton');
+        
+        //Event Bindings
+        $convertbutton.bind('click', function(e){
+            convert($io.value(), e);
+        });
+        
+    });
+    
+})();
